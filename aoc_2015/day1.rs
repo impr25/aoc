d06bdeea7bf400834d8b3948c641cf8a7f0a404c
impr25 <gautamrawat2512@gmail.com>
2024-02-24 23:02:07 +0530

    removed file

--- aoc_2015/day1.rs
+++ aoc_2015/day1.rs
@@ -9,7 +9,7 @@
 fn main() {
     let width1 = 30;
     let height1 = 50;
-    let mut puzzle_in = String::new();
+    let mut puzzle_in = Sting::new();
 
     println!(
         "The area of the rectangle is {} square pixels.",
@@ -20,3 +20,4 @@ fn main() {
 fn area(width: u32, height: u32) -> u32 {
     width * height
 }
+
