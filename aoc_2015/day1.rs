ab9275535cfd898dd79ad46b53c98b897fe8c7cf
impr25 <69866115+impr25@users.noreply.github.com>
2024-02-24 22:23:49 +0530

    Update day1.rs

--- aoc_2015/day1.rs
+++ aoc_2015/day1.rs
@@ -1,7 +1,22 @@
-use std::io;
+// use std::io;
+
+// fn main() {
+//     let mut puzzle_in = String::new();
+//     io::stdin().read_line(&mut puzzle_in).expect("failed to get input");
+//     println!("{}",puzzle_in);
+// }
 
 fn main() {
+    let width1 = 30;
+    let height1 = 50;
     let mut puzzle_in = String::new();
-    io::stdin().read_line(&mut puzzle_in).expect("failed to get input");
-    println!("{}",puzzle_in);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
