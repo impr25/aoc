8a73487e29ae34e3b88d3c58da8bfbd34ccf2709
gauta <gauta@PC2512-7GVEE9PT>
2024-06-22 20:44:46 +0530

    day01 finished

--- aoc_2015/day01/src/main.rs
+++ aoc_2015/day01/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 
 fn main() {
@@ -6,6 +7,42 @@ fn main() {
 
     let file_path= &args[1];
     
-    println!("Current file path {}", file_path);
+    let contents = fs::read_to_string(file_path).expect("File not found");
+    let mut answer: i32 = 0;
+  
+    // for ind in 0..contents.chars().count(){
+    //     if answer == -1{
+    //         println!("negative ind {}",ind);
+    //     }
+    //     if contents.chars().nth(ind).unwrap() =='(' {
+    //         answer += 1;
+    //     }
+    //     else if contents.chars().nth(ind).unwrap() == ')' {
+    //         answer -= 1;            
+    //     }
+    //     else {
+    //         println!("answer {}", answer)
+    //     }
+    // }
 
+    for (ind,char) in contents.chars().enumerate(){
+        if answer == -1{
+            println!("Floor Goes to -1 at: {}", ind);
+        }
+        if char == '('{
+            // print!("{}",char);
+            answer += 1;
+        }
+        else if char == ')'{
+            // print!("{}",char);
+            answer -= 1;
+        }
+        else {
+            println!("New line occured, Previous Answer: {}", answer);
+        }
+    }
+
+    println!("New line occured, Previous Answer: {}", answer);
+    println!("Current file path {}", file_path);
+    // println!("File contains \n{}", contents);
 }
