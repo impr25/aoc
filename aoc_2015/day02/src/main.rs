d03f90403e109dd15b0f29711aefdc8b59ca8a86
gauta <gauta@PC2512-7GVEE9PT>
2024-06-24 02:39:15 +0530

    dayo2 finished

--- aoc_2015/day02/src/main.rs
+++ aoc_2015/day02/src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::fs;
+
+fn calculate_area(dim: &mut Vec<i64>) -> (i64,i64) {
+    dim[..].sort();
+    // println!("{}", dim[0]);
+    return (3*dim[0]*dim[1] + 2*dim[1]*dim[2] + 2*dim[2]*dim[0], 2*(dim[0]+dim[1])+dim[0]*dim[1]*dim[2]);
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path= &args[1];
+    
+    let contents = fs::read_to_string(file_path).expect("File not found");
+
+    let mut area: i64 = 0;
+    let mut length: i64 = 0;
+
+
+    for line in contents.split('\n') {
+        // println!("{}", line);
+
+        // let dim: Vec<&[u8]> = line.split('x').map(|s| s.as_bytes()).collect();
+        // println!("Print is ASCII: {:?},{:?},{:?}",dim[0],dim[1],dim[2]);
+
+        let mut dim: Vec<i64> = line.split('x').map(|s| s.trim().parse::<i64>().unwrap()).collect();
+        area += calculate_area(&mut dim).0;
+        length += calculate_area(&mut dim).1;
+        // println!("{:?},{:?},{:?}",dim[0],dim[1],dim[2])
+        
+    }
+    println!("Total Giftwrap needed: {}",area);
+    println!("Total ribbon needed: {}",length);
 }
